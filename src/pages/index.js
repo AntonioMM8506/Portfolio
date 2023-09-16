6fc19e0254af5da46db4c1fc3b0bf5de126661ce
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2023-09-16 16:28:49 -0600

    Explore

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,9 @@ import { saveAs } from "file-saver"
 import ContentfulImage from "@/components/ui/ContentfulImage";
 import Link from "next/link";
 import Image from "next/image";
-import imageLoader from "@/components/ui/ImageLoader";
 import Skills from "@/components/home/Skills";
 import Projects from "@/components/home/Projects";
-import MailSend from "media/pictures/logo/mail-send.png"
+import Explore from "@/components/home/Explore";
 
 export default function Home() {
   
@@ -93,47 +92,7 @@ export default function Home() {
         </div>
         <br></br>
 
-        {/*Text to redirect to the Blog section*/}
-        <h3 className="text-center font-roboto text-blue-700 font-semibold text-3xl">
-          Explore
-        </h3>
-        <div className="flex">
-          <div className="container py-4 flex flex-col items-center text-center">
-            <span className="px-2 font-roboto text-lg">
-              When I'm not coding, you can find me reading, drawing or studying new things, As one of my teachers 
-              once told me "Sharing your knowledge is a privilege", so I decided to start a Blog, you can give it a 
-              look <a className="underline font-semibold" href="/posts">here </a>
-            </span>
-            <Link href="/posts">
-              <ContentfulImage
-                src="media/pictures/logo/book.png"
-                width="120"
-                height="120"
-                alt='Logo'
-              />
-            </Link>
-          </div>
-
-          {/*Text to redirect to the Contact section*/}
-          <div className="container py-4 flex flex-col items-center text-center">
-            <span className="px-2 font-roboto text-lg">
-              Do you have any questions or just want to share some opinion?. You can visit 
-              my <a className="underline font-semibold" href="https://www.linkedin.com/in/antonio-man-mal/">LinkedIn</a> profile 
-              or you can send me an email <a className="underline font-semibold" href="/posts">here</a>. I will be pleased 
-              to know about you. Thank you for reaching out!
-            </span>
-            <Link href="/contact">
-              <Image
-                loader={imageLoader}
-                src={MailSend}
-                width="140"
-                height="140"
-                quality="60"
-                alt='Logo'
-              />
-            </Link>
-          </div>
-        </div>
+        <Explore/>
 
 
 
