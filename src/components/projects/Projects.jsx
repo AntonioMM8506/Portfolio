e7a7ebffb3a8faff21781144b5f007dbd6ea12be
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2025-01-27 21:14:24 -0600

    Projects refactor

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import ImageWithLoader from "../layout/imageWithLoader";
 
 const MyProjects = () => {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:1234/";
-
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  //const baseUrl = "http://localhost:1234/";
 
     return(
         <div>
