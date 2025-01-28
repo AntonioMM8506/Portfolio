67c8075ae07f3ad3ada02e817899333d662824a5
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2025-01-27 18:45:53 -0600

    Image Loader

--- src/components/layout/imageWithLoader.jsx
+++ src/components/layout/imageWithLoader.jsx
@@ -1,28 +1,24 @@
+import Image from "next/image";
 import React, { useState } from "react";
-import { useInView } from "react-intersection-observer";
 
-const ImageWithLoader = ({ src, alt, className }) => {
+const ImageWithLoader = ({ src, alt, className, width, height }) => {
     const [loading, setLoading] = useState(true);
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-    });
 
     return (
-        <div className="relative" ref={ref}>
-        {loading && inView && (
+        <div className="relative" style={{ width, height }}>
+        {loading && (
             <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
             <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
             </div>
         )}
-        {inView && (
-            <img
+        <Image
             src={src}
             alt={alt}
+            width={width}
+            height={height}
             className={`${className} ${loading ? "opacity-0" : "opacity-100"} transition-opacity duration-300`}
-            onLoad={() => setLoading(false)}
-            />
-        )}
+            onLoadingComplete={() => setLoading(false)}
+        />
         </div>
     );
 };
