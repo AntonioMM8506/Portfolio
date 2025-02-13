6f91bde6f39fe87c1cb68ed4811d18e937886a50
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2025-02-12 20:38:16 -0600

    Lytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,17 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
-
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Lytics Tracking Script */}
-        <script src="/lytics.js" type="text/javascript" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+<script
+  type="text/javascript"
+  dangerouslySetInnerHTML={{
+    __html: `
+      !function(){"use strict";var o=window.jstag||(window.jstag={}),r=[];function n(e){o[e]=function(){for(var n=arguments.length,t=new Array(n),i=0;i<n;i++)t[i]=arguments[i];r.push([e,t])}}n("send"),n("mock"),n("identify"),n("pageView"),n("unblock"),n("getid"),n("setid"),n("loadEntity"),n("getEntity"),n("on"),n("once"),n("call"),o.loadScript=function(n,t,i){var e=document.createElement("script");e.async=!0,e.src=n,e.onload=t,e.onerror=i;var o=document.getElementsByTagName("script")[0],r=o&&o.parentNode||document.head||document.body,c=o||r.lastChild;return null!=c?r.insertBefore(e,c):r.appendChild(e),this},o.init=function n(t){return this.config=t,this.loadScript(t.src,function(){if(o.init===n)throw new Error("Load error!");o.init(o.config),function(){for(var n=0;n<r.length;n++){var t=r[n][0],i=r[n][1];o[t].apply(o,i)}r=void 0}()}),this}}();
+      // Define config and initialize Lytics tracking tag.
+      jstag.init({
+        src: 'https://c.lytics.io/api/tag/9a0c6f89f493ba1d0d6c6b052e3c72cb/latest.min.js',
+        pageAnalysis: {
+          dataLayerPull: {
+            disabled: true
+          }
+        }
+      });
+    `,
+  }}
+/>
