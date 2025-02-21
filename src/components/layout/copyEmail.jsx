1afb7aac507197f2266e929af8db39e502d658d6
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2025-02-20 21:14:10 -0600

    BASE

--- src/components/layout/copyEmail.jsx
+++ src/components/layout/copyEmail.jsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 const CopyEmail = () => {
 
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "" || "https://localhost:1234/";
 
     const email = "amaldonadodev@protonmail.com";
 
